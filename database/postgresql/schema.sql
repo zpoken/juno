ca663059d3b9668c11d615ed746bb790b8d1c5f0
Magic Cat <pusz.monika@gmail.com>
2022-05-13 14:09:53 +0100

    updated desc

--- database/postgresql/schema.sql
+++ database/postgresql/schema.sql
@@ -79,7 +79,7 @@ CREATE INDEX message_involved_accounts_index ON message USING GIN(involved_accou
 CREATE INDEX message_value_index ON message USING GIN(value);
 
 /**
- * This function is used to find all the utils that involve any of the given addresses and have
+ * This function is used to find all messages that involve the given address and have
  * type that is one of the specified types.
  */
 CREATE FUNCTION messages_by_address(
