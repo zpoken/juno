e7e0cccd33fd4d613545f73688f43b80f364b9fa
Magic Cat <pusz.monika@gmail.com>
2022-07-08 16:43:01 +0200

    add TxResponse type

--- types/nomic.go
+++ types/nomic.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/gogo/protobuf/types"
+)
+
 type TxResponseTest struct {
 	Fee        BDCoin         `protobuf:"bytes,1,opt,name=fee,proto3" json:"fee,omitempty"`
 	Memo       string         `protobuf:"bytes,2,opt,name=memo,proto3" json:"memo,omitempty"`
@@ -47,4 +52,35 @@ func NewTxResponseTest(
 		Msg:        msg,
 		Signatures: sig,
 	}
+}
+
+type TxResponse struct {
+	// The block height
+	Height string `protobuf:"varint,1,opt,name=height,proto3" json:"height,omitempty"`
+	// The transaction hash.
+	Hash string `protobuf:"bytes,2,opt,name=hash,proto3" json:"hash,omitempty"`
+	// Namespace for the Code
+	Codespace string `protobuf:"bytes,3,opt,name=codespace,proto3" json:"codespace,omitempty"`
+	// Response code.
+	Code uint32 `protobuf:"varint,4,opt,name=code,proto3" json:"code,omitempty"`
+	// Result bytes, if any.
+	Data string `protobuf:"bytes,5,opt,name=data,proto3" json:"data,omitempty"`
+	// The output of the application's logger (raw string). May be
+	// non-deterministic.
+	RawLog string `protobuf:"bytes,6,opt,name=raw_log,json=rawLog,proto3" json:"raw_log,omitempty"`
+	// The output of the application's logger (typed). May be non-deterministic.
+	Logs sdk.ABCIMessageLogs `protobuf:"bytes,7,rep,name=logs,proto3,castrepeated=ABCIMessageLogs" json:"logs"`
+	// Additional information. May be non-deterministic.
+	Info string `protobuf:"bytes,8,opt,name=info,proto3" json:"info,omitempty"`
+	// Amount of gas requested for transaction.
+	GasWanted int64 `protobuf:"varint,9,opt,name=gas_wanted,json=gasWanted,proto3" json:"gas_wanted,omitempty"`
+	// Amount of gas consumed by transaction.
+	GasUsed int64 `protobuf:"varint,10,opt,name=gas_used,json=gasUsed,proto3" json:"gas_used,omitempty"`
+	// The request transaction bytes.
+	Tx *types.Any `protobuf:"bytes,11,opt,name=tx,proto3" json:"tx,omitempty"`
+	// Time of the previous block. For heights > 1, it's the weighted median of
+	// the timestamps of the valid votes in the block.LastCommit. For height == 1,
+	// it's genesis time.
+	Timestamp string `protobuf:"bytes,12,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+	JsonRPC   string `protobuf:"bytes,13,opt,name=jsonrpc,proto3" json:"jsonrpc,omitempty"`
 }
